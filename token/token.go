2f6181b2f2bbce77301b9ba3498978993c033ed8
Mauricio Massa <mmassa2137@outlook.com>
2024-01-15 19:35:00 -0400

    add lexer, token & irules

---
+++ token/token.go
@@ -0,0 +1,75 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y
+	INT   = "INT"   // 12345
+
+	// F5 Event Contexts
+	// when <EVENT_CONTEXT> {}
+	HTTP_REQUEST        = "HTTP_REQUEST"
+	HTTP_RESPONSE       = "HTTP_RESPONSE"
+	CLIENTSSL_HANDSHAKE = "CLIENTSSL_HANDSHAKE"
+	SERVERSSL_HANDSHAKE = "SERVERSSL_HANDSHAKE"
+	LB_SELECTED         = "LB_SELECTED"
+	LB_FAILED           = "LB_FAILED"
+	TCP_REQUEST         = "TCP_REQUEST"
+
+	//operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
