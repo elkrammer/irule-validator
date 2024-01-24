78fe7e7f599a06edb592964140439ec194d2caee
Mauricio Massa <mmassa2137@outlook.com>
2024-01-24 19:46:53 -0400

    string literal

---
+++ ast/ast_test.go
@@ -0,0 +1,22 @@
+package ast
+
+// import (
+// "testing"
+//
+// "github.com/elkrammer/irule-validator/token"
+// )
+
+// func TestString(t *testing.T) {
+// 	program := &Program{
+// 		Statements: []Statement{
+// 			Value: &Identifier{
+// 				Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+// 				Value: "anotherVar",
+// 			},
+// 		},
+// 	}
+//
+// 	if program.String() != "anotherVar;" {
+// 		t.Errorf("program.String() wrong. Got=%q", program.String())
+// 	}
+// }
