6b91cdd58d36083f43fd76f63d1008b9876df79d
Mauricio Massa <mmassa2137@outlook.com>
2024-01-28 09:11:25 -0400

    ast tests

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -1,22 +1,29 @@
 package ast
 
-// import (
-// "testing"
-//
-// "github.com/elkrammer/irule-validator/token"
-// )
+import (
+	"testing"
 
-// func TestString(t *testing.T) {
-// 	program := &Program{
-// 		Statements: []Statement{
-// 			Value: &Identifier{
-// 				Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
-// 				Value: "anotherVar",
-// 			},
-// 		},
-// 	}
-//
-// 	if program.String() != "anotherVar;" {
-// 		t.Errorf("program.String() wrong. Got=%q", program.String())
-// 	}
-// }
+	"github.com/elkrammer/irule-validator/token"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&ReturnStatement{
+				Token: token.Token{Type: token.RETURN, Literal: "return"},
+				ReturnValue: &Identifier{
+					Token: token.Token{Type: token.STRING, Literal: "true"},
+					Value: "true",
+				},
+				// Value: &Identifier{
+				// 	Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+				// 	Value: "anotherVar",
+				// },
+			},
+		},
+	}
+
+	if program.String() != "return true;" {
+		t.Errorf("program.String() wrong. Got=%q", program.String())
+	}
+}
