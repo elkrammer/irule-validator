eedf9ee31585883ba1aa4797bccec7352d8ced66
Mauricio Massa <mmassa2137@outlook.com>
2024-05-15 15:03:52 -0400

    add missing cases

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -100,6 +100,10 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.GT, l.ch)
 	case '*':
 		tok = newToken(token.ASTERISK, l.ch)
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
 	case 0:
 		tok.Type = token.EOF
 		tok.Literal = ""
