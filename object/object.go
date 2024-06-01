d008e634557a73b621197e24d4f56116b87e175d
Mauricio Massa <mmassa2137@outlook.com>
2024-05-31 21:18:28 -0400

    initial commit of object system

---
+++ object/object.go
@@ -0,0 +1,37 @@
+package object
+
+import (
+	"fmt"
+)
+
+type ObjectType string
+
+const (
+	NUMBER_OBJ  = "NUMBER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+)
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Number struct {
+	Value float64
+}
+
+func (i *Number) Type() ObjectType { return NUMBER_OBJ }
+func (i *Number) Inspect() string  { return fmt.Sprintf("%f", i.Value) }
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+type Null struct{}
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return "" }
