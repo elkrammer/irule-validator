25c6be77117ca6604327798429738e13b88dd2c8
Mauricio Massa <mmassa2137@outlook.com>
2024-07-06 09:03:20 -0400

    eval string concat

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -202,6 +202,11 @@ func TestErrorHandling(t *testing.T) {
 			"identifier not found: foobar",
 			false,
 		},
+		{
+			`"hello" - "world"`,
+			"unknown operator: STRING - STRING",
+			false,
+		},
 	}
 
 	for _, tt := range tests {
@@ -315,6 +320,20 @@ func TestStringLiteral(t *testing.T) {
 	}
 
 	if str.Value != "howdy!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
 		t.Errorf("String has wrong value. got=%q", str.Value)
 	}
 }
